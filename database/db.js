82e4123b4ef0cf03998a6070be9dbd0d43690433
ZaaXxyZzModDev <130498570+ZaaXxyZzModDev@users.noreply.github.com>
2023-08-06 07:40:39 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://kuhong-api.herokuapp.com';
+var url = 'https://api-zaaxxyzz.my-web.repl.co';
 try {
-if(url == 'https://kuhong-api.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api-zaaxxyzz.my-web.repl.co') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}

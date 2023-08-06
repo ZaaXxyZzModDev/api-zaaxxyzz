bf209afb8db5a584dc40ca8c92ae693f491e5804
ZaaXxyZzModDev <130498570+ZaaXxyZzModDev@users.noreply.github.com>
2023-08-06 07:56:43 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -43,8 +43,8 @@ var port = process.env.PORT || 8080 || 5000 || 3000
         uptime: muptime(process.uptime()),
         speed: `${neww - old}ms`,
         info:{       
-            owner: 'alpin',            
-            apikey: 'Chat Owner: https://wa.me/6289690719275'
+            owner: 'ZaaXxyZz',            
+            apikey: 'Chat Owner: https://wa.me/6283823911619'
         }
     }
     res.json(status)
